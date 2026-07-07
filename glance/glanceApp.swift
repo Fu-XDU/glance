3228acb6da98f5bb71c89f046a327f92a90e350d
mingfu <mingfu@tencent.com>
2026-07-07 20:31:58 +0800

    first commit

---
+++ glance/glanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  glanceApp.swift
+//  glance
+//
+//  Created by mingfu on 2026/7/7.
+//
+
+import SwiftUI
+
+@main
+struct glanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ glanceTests/glanceTests.swift
@@ -0,0 +1,19 @@
+//
+//  glanceTests.swift
+//  glanceTests
+//
+//  Created by mingfu on 2026/7/7.
+//
+
+import Testing
+@testable import glance
+
+struct glanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
